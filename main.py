89738d154f4a8f363de76d5216936128fa49e2d4
sunshine <1130018964@qq.com>
2019-03-17 22:06:32 +0800

    优化

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
+
 from pygame.locals import *
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+
 pygame.init()
-running = True
 
 screen = pygame.display.set_mode((800, 600))
-surf = pygame.Surface((50, 50))
-surf.fill((255, 255, 255))
-rect = surf.get_rect()
-screen.blit(surf,(400,300))
+
+running = True
+
+player = Player()
+
+screen.blit(player.surf, player.rect)
+
 pygame.display.flip()
+
 while running:
     for event in pygame.event.get():
-
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
